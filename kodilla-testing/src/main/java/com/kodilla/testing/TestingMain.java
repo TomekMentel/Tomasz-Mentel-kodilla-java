8de38841c7b8f15f3554c9c122b514ca35b8aae1
unknown <mentell@o2.pl>
2019-04-11 15:59:54 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,50 @@
 package com.kodilla.testing;
 
-public class TestingMain {
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+public class TestingMain {
+    public static void main(String[] args) {
+
+        // SimpleUser Testing
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // Testing Calculator Class
+
+        Calculator calculator = new Calculator();
+        int addResults = calculator.add(23, 17);
+        int subResults = calculator.subtract(23, 13);
+        if ((addResults == 40) && (subResults == 10)) {
+            System.out.println("Calculator Test OK");
+        } else {
+            System.out.println("Calculator Test Error!");
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
