b648d8a35b927aa4b435c6943dbc2da05938069c
Tomasz Mentel <mentell@o2.pl>
2019-07-10 15:06:21 +0200

    Modul 15.5 Bigmac ingredients Test v.1

--- kodilla-patterns/src/main/test/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/main/test/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -2,8 +2,6 @@ package com.kodilla.patterns.builder.bigmac;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 public class BigmacTestSuite {
@@ -22,14 +20,16 @@ public class BigmacTestSuite {
         //When
         int howManyIngredients = bigmac.getIngredients().size();
         int howManySauce = bigmac.getSauce().size();
-        List<String> ingredientTest = bigmac.getIngredients();
-        List<String> sauceTest = bigmac.getSauce();
         //Then
         assertEquals(3, howManyIngredients);
         assertEquals(2, howManySauce);
         assertEquals("Z sezamem", bigmac.getBuns());
-        assertEquals(ingredientTest, bigmac.getIngredients());
-        assertEquals(sauceTest, bigmac.getSauce());
+        assertEquals("Bekon", bigmac.getIngredients().get(0));
+        assertEquals("Cebulka", bigmac.getIngredients().get(1));
+        assertEquals("Szynka", bigmac.getIngredients().get(2));
+
+        assertEquals("Barbecue", bigmac.getSauce().get(0));
+        assertEquals("Standard", bigmac.getSauce().get(1));
         assertEquals(2, bigmac.getBurgers());
     }
 }
