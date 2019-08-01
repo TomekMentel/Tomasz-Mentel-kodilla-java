11345dc80bb6f76c57607a7a53e970498ae4dac4
Tomasz Mentel <mentell@o2.pl>
2019-08-01 20:07:19 +0200

    modul 17.3.

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Item.java
@@ -0,0 +1,86 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ITEMS")
+public class Item {
+    private int Id;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal value;
+    private Product product;
+    private Invoice invoice;
+
+    public Item() {
+    }
+
+    public Item(BigDecimal price, int quantity, BigDecimal value) {
+        this.price = price;
+        this.quantity = quantity;
+        this.value = value;
+
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    @NotNull
+    @Column(name = "PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @NotNull
+    @Column(name = "VALUE")
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "INVOICE_ID")
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,69 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    InvoiceDao invoiceDao;
+    @Autowired
+    ProductDao productDao;
+    @Autowired
+    ItemsDao itemsDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Product product1 = new Product("Widelec");
+        Product product2 = new Product("Lyzka");
+
+        Item item1 = new Item(new BigDecimal(12), 30, new BigDecimal(360));
+        Item item2 = new Item(new BigDecimal(15), 25, new BigDecimal(375));
+        Item item3 = new Item(new BigDecimal(30), 20, new BigDecimal(300));
+        Item item4 = new Item(new BigDecimal(18), 15, new BigDecimal(270));
+
+        product1.getItems().add(item1);
+        product1.getItems().add(item2);
+        product2.getItems().add(item3);
+        product2.getItems().add(item4);
+
+        item1.setProduct(product1);
+        item2.setProduct(product1);
+        item3.setProduct(product2);
+        item4.setProduct(product2);
+
+        Invoice invoice = new Invoice("1/1/2010");
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+        item3.setInvoice(invoice);
+        item4.setInvoice(invoice);
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+        productDao.save(product1);
+        productDao.save(product2);
+        int id1 = product1.getId();
+        int id2 = product2.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+        Assert.assertNotEquals(0, id1);
+        Assert.assertNotEquals(0, id2);
+
+        //CleanUp
+        invoiceDao.deleteById(id);
+        productDao.deleteById(id1);
+        productDao.deleteById(id2);
+    }
+}
