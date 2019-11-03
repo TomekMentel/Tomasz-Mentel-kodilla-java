c696770325712b5bf329efc6c44797260eaf8619
Tomasz Mentel <mentell@o2.pl>
2019-11-03 13:58:38 +0100

    modul 21.1

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/KodillaPatterns2Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.patterns2;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class KodillaPatterns2Application {
+
+    public static void main(String[] args) {
+        run(KodillaPatterns2Application.class, args);
+    }
+}
